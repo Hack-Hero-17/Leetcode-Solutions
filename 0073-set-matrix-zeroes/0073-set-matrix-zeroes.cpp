046ceb4d6e5cc33c658c5da593893e8626628d8b
Hariharan R I <hariharan17kpm@gmail.com>
2025-05-21 20:17:50 +0530

    Time: 0 ms (100%), Space: 18.6 MB (52.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,8 +3,7 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         bool zeroinFirstCol = false;
         for (int row = 0; row < matrix.size(); row++) {
-            if (matrix[row][0] == 0)
-                zeroinFirstCol = true;
+            if (matrix[row][0] == 0) zeroinFirstCol = true;
             for (int col = 1; col < matrix[0].size(); col++) {
                 if (matrix[row][col] == 0) {
                     matrix[row][0] = 0;
