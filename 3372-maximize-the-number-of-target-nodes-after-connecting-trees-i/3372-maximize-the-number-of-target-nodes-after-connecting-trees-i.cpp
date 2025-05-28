3dc10fdc9d64d1b829c2a040074e6855aadd65a3
Hariharan R I <hariharan17kpm@gmail.com>
2025-05-28 19:01:45 +0530

    Time: 101 ms (97.7%), Space: 59.1 MB (100%) - LeetHub

--- 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-    vector<vector<int>> buildList(const vector<vector<int>>& edges) {
-        vector<vector<int>> adj(edges.size() + 1);
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-        return adj;
-    }
-    
-    int dfs(const vector<vector<int>>& adj, int u, int p, int k) {
-        if (k < 0) return 0;
-        int cnt = 1;
-        for (int v : adj[u])
-            if (v != p) cnt += dfs(adj, v, u, k-1);
-        return cnt;
-    }
-    
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        auto adj1 = buildList(edges1), adj2 = buildList(edges2);
-        int n = adj1.size(), m = adj2.size(), maxiB = 0;
-        vector<int> res(n);
 
-        for (int i = 0; i < m; i++) maxiB = max(maxiB, dfs(adj2, i, -1, k - 1));
-        for (int i = 0; i < n; i++) res[i] = dfs(adj1, i, -1, k) + maxiB;
-        return res;
+    int DFS(int node, int par, int dist, vector<vector<int>>& edge){
+        if(dist < 0){
+            return 0;
+        }
+
+        int cr = 1;
+
+        for(auto adj : edge[node]){
+            if(adj != par){
+                cr += DFS(adj, node, dist-1, edge);
+            }
+        }
+
+        return cr;
+    }
+    
+    vector<int> maxTargetNodes(vector<vector<int>>& e1, vector<vector<int>>& e2, int k) {
+        int n = e1.size()+1;
+        int m = e2.size()+1;
+        
+        vector<vector<int>> edge1(n);
+        vector<vector<int>> edge2(m);
+
+        for(int i=0; i<n-1; i++){
+            edge1[e1[i][0]].push_back(e1[i][1]);
+            edge1[e1[i][1]].push_back(e1[i][0]);
+        }
+
+        for(int i=0; i<m-1; i++){
+            edge2[e2[i][0]].push_back(e2[i][1]);
+            edge2[e2[i][1]].push_back(e2[i][0]);
+        }
+
+        int mxIn2 = 0;
+        for(int i=0; i<m; i++){
+            int ifInode = DFS(i,-1,k-1,edge2);
+            mxIn2 = max(mxIn2, ifInode);
+        }
+
+        vector<int> ans(n);
+
+        for(int i=0; i<n; i++){
+            int cur = DFS(i,-1,k,edge1);
+            ans[i] = cur + mxIn2;
+        }
+
+        return ans;
     }
 };
