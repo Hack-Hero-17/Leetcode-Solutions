47e9e7304967a51b371207bf22fc7c26096f9de0
Hariharan R I <hariharan17kpm@gmail.com>
2025-06-01 19:29:16 +0530

    Time: 551 ms (5.63%), Space: 71.6 MB (80.71%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum = 0;
-        int maxi = INT_MIN;
-        for( int i=0 ; i<nums.size() ; i++ ){
-            sum += nums[i];
-            if( sum > maxi ){
-                maxi = sum;
-            }
-            if( sum < 0 ){
-                sum = 0;
-            }
+    int maxSubArray(vector<int>& arr) {
+        int n = arr.size();
+        long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
 
+    int start = 0;
+    int ansStart = -1, ansEnd = -1;
+    for (int i = 0; i < n; i++) {
+
+        if (sum == 0) start = i; // starting index
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+
+            ansStart = start;
+            ansEnd = i;
         }
-        return maxi;
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    //printing the subarray:
+    cout << "The subarray is: [";
+    for (int i = ansStart; i <= ansEnd; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "]n";
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
     }
 };
