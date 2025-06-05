4b3a900531c975f5b20e5b1afa19e52f2aad8448
Hariharan R I <hariharan17kpm@gmail.com>
2025-06-05 20:23:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-     void fun(vector<int>& nums, int index, vector<int>& ds, set<vector<int>>& res) {
-        if (index == nums.size()) {
-            res.insert(ds); // Insert the current subset
-            return;
-        }
-        
-        // Include the current element
-        ds.push_back(nums[index]);
-        fun(nums, index + 1, ds, res);
-        ds.pop_back(); // Backtrack
-        
-        // Skip duplicates: only process the next element if it's different from the current one
-        while (index + 1 < nums.size() && nums[index] == nums[index + 1]) {
-            index++;
-        }
-        
-        // Do not include the current element
-        fun(nums, index + 1, ds, res);
-    }
+    void findSubsets(int ind, vector < int > & nums, vector < int > & ds, vector < vector < int >> & ans) {
+         ans.push_back(ds);
+         for (int i = ind; i < nums.size(); i++) {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            ds.push_back(nums[i]);
+            findSubsets(i + 1, nums, ds, ans);
+            ds.pop_back();
+         }
     
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); // Sort the input array to handle duplicates correctly
-        vector<int> ds;
-        set<vector<int>> res;
-        vector<vector<int>> ans;
-        
-        fun(nums, 0, ds, res);
-        
-        // Convert the set to a vector
-        for (auto& subset : res) {
-            ans.push_back(subset);
-        }
-        
-        return ans;
-    }
+    vector < vector < int >> subsetsWithDup(vector < int > & nums) {
+         vector < vector < int >> ans;
+         vector < int > ds;
+         sort(nums.begin(), nums.end());
+         findSubsets(0, nums, ds, ans);
+         return ans;
+      }
 };
