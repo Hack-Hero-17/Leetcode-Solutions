fbb22e7d99a725b73a02c1db2ab2dbb654192d29
Hariharan R I <hariharan17kpm@gmail.com>
2025-06-07 15:47:36 +0530

    Time: 4 ms (27.19%), Space: 9 MB (29.02%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        vector<vector<string>> output;
+        vector<string> NQueens( n , string(n,'.'));
+        solve( n , output , NQueens , 0 );
+        return output.size();
+    }
+    void solve( int n , vector<vector<string>>& output , vector<string>& NQueens , int row ){
+        if( row == n){
+            output.push_back(NQueens);
+            return;
+        }
+        for( int col = 0 ; col <n ; col++){
+            if( isSafe( n ,NQueens, row , col )){
+                NQueens[row][col] = 'Q';
+                solve( n , output, NQueens , row+1);
+                NQueens[row][col] = '.';
+            }
+        }
+    }
+    bool isSafe( int n , vector<string>& NQueens , int row , int col ){
+        for( int i = 0 ; i<=row ; i++ ){
+            if( NQueens[i][col] == 'Q' )
+                return false;
+        }
+
+        for(int  i=row-1 , j = col-1 ; i>=0 && j>=0 ; i-- , j-- ){
+            if( NQueens[i][j] == 'Q' )
+                return false;
+        }
+
+        for(int  i= row-1 ,  j = col+1 ; i>=0 && j<n ; i-- , j++ ){
+            if( NQueens[i][j] == 'Q' )
+                return false;
+        }
+
+        return true;
+    }
+};
