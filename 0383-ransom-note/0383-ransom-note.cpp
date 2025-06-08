530695ee85e4d1cf66771cff7402d556554334c7
Hariharan R I <hariharan17kpm@gmail.com>
2025-06-08 20:34:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mg;
+
+        for( int i=0 ; i<magazine.length() ; i++ ){
+            mg[magazine[i]]++;
+        }
+
+        for( int i=0 ; i<ransomNote.length() ; i++ ){
+            if( --mg[ransomNote[i]]<0)
+                return false;
+        }
+        return true;
+    }
+};
