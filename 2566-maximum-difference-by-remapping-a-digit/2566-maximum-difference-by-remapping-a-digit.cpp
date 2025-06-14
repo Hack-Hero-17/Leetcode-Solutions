27b229652ec8fa2af0def26d9716c1d582625e91
Hariharan R I <hariharan17kpm@gmail.com>
2025-06-14 18:35:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (37.74%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
@@ -7,21 +7,25 @@ public:
         int n = str.size();
         char ele = ' ';
         // Step 1: Find first non-9 digit for max replacement
-        for(int i=0;i<n;i++){
-            if(str[i] != '9'){
+        for (int i = 0; i < n; i++) {
+            if (str[i] != '9') {
                 ele = str[i];
                 break;
             }
         }
         // Step 2: Replace that digit with 9 for max
-        for(int i=0;i<n;i++){
-            if(ele == str[i]) ma += '9';
-            else ma += str[i];
+        for (int i = 0; i < n; i++) {
+            if (ele == str[i])
+                ma += '9';
+            else
+                ma += str[i];
         }
         // Step 3: Replace first digit for min with 0
-        for(int i=0;i<n;i++){
-            if(str[0] == str[i]) mi += '0';
-            else mi += str[i];
+        for (int i = 0; i < n; i++) {
+            if (str[0] == str[i])
+                mi += '0';
+            else
+                mi += str[i];
         }
         // Step 4: Convert and calculate result
         return stoi(ma) - stoi(mi);
