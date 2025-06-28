a8f95e6e58236dbaad536ef6d16cf2fbd075c07c
Hariharan R I <hariharan17kpm@gmail.com>
2025-06-28 22:04:16 +0530

    Time: 115 ms (5.09%), Space: 34.5 MB (5.17%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        unordered_set<string> visited;
+
+        vector<pair<int,int>> directions = { {1,0} , {-1,0} , {0,1} , {0,-1} };
+
+        for( int i=0 ; i<rows ; i++ ){
+            for( int j=0 ; j<cols ; j++ ){
+                if( grid[i][j] == '1' && visited.find(to_string(i) + "," + to_string(j)) == visited.end() ){
+                    islands++;
+                    bfs( grid , i , j , visited, directions, rows, cols );
+                }
+            }
+        }
+        return islands;
+    }
+    void bfs( vector<vector<char>>& grid , int i , int j , unordered_set<string>& visited , vector<pair<int,int>> directions, int rows, int cols ){
+        queue<pair<int,int>> q;
+        q.push( {i,j} );
+        visited.insert( to_string(i) + "," + to_string(j) );
+
+        while( !q.empty() ){
+            auto [r,c] = q.front();
+            q.pop();
+            for( auto[dr,dc] : directions ){
+                int nr = r+dr;
+                int nc = c+dc;
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && visited.find(to_string(nr) + "," + to_string(nc)) == visited.end()) {
+                    q.push({nr, nc});
+                    visited.insert(to_string(nr) + "," + to_string(nc));
+                }
+            }
+        }
+    }
+};
