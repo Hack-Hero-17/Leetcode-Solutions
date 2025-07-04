d039447c16791435ec29a74e2ff7eb348729a3d0
Hariharan R I <hariharan17kpm@gmail.com>
2025-07-04 20:06:17 +0530

    Time: 1 ms (7.2%), Space: 13.7 MB (5.25%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,19 +11,19 @@
  */
 class Solution {
     vector<string> ans;
-    void trav(TreeNode* root, string s, string prev){
+    void trav(TreeNode* root, string s){
         if(!root) return;
-        prev = s;
+        // prev = s;
         s = s + to_string(root->val);
         if(!root->left && !root->right) ans.push_back(s);
-        trav(root->left, s, prev);
-        trav(root->right, s, prev);
-        s = prev;
+        trav(root->left, s);
+        trav(root->right, s);
+        // s = prev;
     }
 public:
     int sumNumbers(TreeNode* root) {
         string s = "", prev= "";
-        trav(root, s, prev);
+        trav(root, s);
         int sol = 0;
         for(auto n: ans)  sol += stoi(n);
         return sol;
