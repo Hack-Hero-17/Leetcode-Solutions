ea874b6c9337c4e008c4e57d7c435780b3ca9386
Hariharan R I <hariharan17kpm@gmail.com>
2025-07-07 19:44:51 +0530

    Time: 62 ms (59.15%), Space: 75 MB (64.96%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,27 +1,29 @@
 class Solution {
-    public:
-     int maxEvents(int[][] events) {
-        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        // Sort events based on start day
+        sort(events.begin(), events.end());
         
-        int day = 0, index = 0 , n = events.length ,result = 0;      
+        priority_queue<int, vector<int>, greater<int>> minHeap;
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        while (!pq.isEmpty() || index < n) {
-            if (pq.isEmpty()) {
+        int day = 0, index = 0, n = events.size(), result = 0;
+        
+        while (!minHeap.empty() || index < n) {
+            if (minHeap.empty()) {
                 day = events[index][0];
             }
             while (index < n && events[index][0] <= day) {
-                pq.offer(events[index][1]);
+                minHeap.push(events[index][1]);
                 index++;
             }
-            pq.poll();
-            result++; 
-            day++;    
-
-            while (!pq.isEmpty() && pq.peek() < day) {
-                pq.poll();
+            minHeap.pop();
+            result++;
+            day++;
+            
+            while (!minHeap.empty() && minHeap.top() < day) {
+                minHeap.pop();
             }
         }
         return result;
     }
-}
+};
