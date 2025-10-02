ad09099e0129d3037df0f6dc17e0a3abd7696cce
Hariharan R I <hariharan17kpm@gmail.com>
2025-10-02 19:41:01 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.66%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.cpp
+++ 3100-water-bottles-ii/3100-water-bottles-ii.cpp
@@ -1,1 +1,15 @@
-numBottles
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int bottleDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange;
+            numExchange++;
+            bottleDrunk++;
+            emptyBottles++;
+        }
+        return bottleDrunk;
+    }
+};
