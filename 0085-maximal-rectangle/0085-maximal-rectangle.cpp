cf0efcf7d01b0bd3a77d8cdc420ee5afb56f0186
Hariharan R I <hariharan17kpm@gmail.com>
2026-01-11 19:57:42 +0530

    Time: 3 ms (84.26%), Space: 16 MB (98.7%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,50 +1,34 @@
+short st[201]={-1}, top=0;
 class Solution {
 public:
-    int maxRect(vector<int>& mat) {
-        int n = mat.size();
-        stack<pair<int, int>> st;
-        int maxi = 0;
+    static int maximalRectangle(vector<vector<char>>& matrix) {
+        const unsigned short row=matrix.size(), col=matrix[0].size();
+        if (row==1 && col==1) return matrix[0][0]=='1';
+        unsigned short h[201]={0};//height 
+        int maxArea=0;
 
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && st.top().second > mat[i]) {
-                int next = i - 1;
-                int val = st.top().second;
-                st.pop();
-                int prev = st.empty() ? -1 : st.top().first;
-                maxi = max(maxi, ((next - prev) * val));
-            }
-            st.push({i, mat[i]});
-        }
-        while (!st.empty()) {
-            int next = n - 1;
-            int val = st.top().second;
-            st.pop();
-            int prev = st.empty() ? -1 : st.top().first;
-            maxi = max(maxi, ((next - prev) * val));
-        }
-        return maxi;
-    }
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> histMat(n, vector<int>(m, 0));
-        for (int i = 0; i < m; i++) {
-            histMat[0][i] = matrix[0][i] - '0';
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == '0') {
-                    histMat[i][j] = 0;
-                    continue;
+        for(int i=0; i<row; i++){
+            top=0;//stack will not be empty & st[0]=-1
+            for (int j=0; j<=col; j++){
+                // Count the successive '1's & store in h[j]
+                h[j]=(j==col||matrix[i][j]=='0')?0:h[j]+1;
+
+                // monotonic stack has at least element -1
+                while(top>0 && (j==col||h[j]<h[st[top]])){
+                    const int m=st[top--];//pop
+                    const int w=j-st[top]-1;
+                    const int area=h[m]*w;
+                    maxArea=max(maxArea, area);
                 }
-                histMat[i][j] = histMat[i - 1][j] + (matrix[i][j] - '0');
+                st[++top]=j;//push
             }
         }
-        int maxi = 0;
-        for (int i = 0; i < n; i++) {
-            int val = maxRect(histMat[i]);
-            maxi = max(maxi, val);
-        }
-        return maxi;
+        return maxArea;
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
