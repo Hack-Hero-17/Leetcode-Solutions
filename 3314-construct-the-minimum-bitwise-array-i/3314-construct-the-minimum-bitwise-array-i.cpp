d823ef2b617691eae6d136ca30af31a2ff0d611d
Hariharan R I <hariharan17kpm@gmail.com>
2026-01-20 19:39:44 +0530

    Time: 0 ms (100%), Space: 25 MB (78.7%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -1,9 +1,1 @@
-class Solution {
-public:
-        vector<int> minBitwiseArray(vector<int>& nums) {
-        for (int& n : nums) {
-            n = n ^ (((n + 1) & ~n) >> 1) | ((n & 1) - 1);
-        }
-        return move(nums);
-    }
-};
+nums
