f9ab00160e2b21079b4643243413f768e4b88d8c
Hariharan R I <hariharan17kpm@gmail.com>
2026-02-21 23:22:21 +0530

    Time: 0 ms (100%), Space: 8 MB (61.81%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,11 +1,15 @@
+// new variant
 class Solution {
 public:
-    int countPrimeSetBits(int left, int right) {
-        int cnt = 0;
-        while(left <= right){            
-            cnt += 665772 >> __builtin_popcount(left++) & 1;
+    static int countPrimeSetBits(int left, int right) {
+        int prime[]={2, 3, 5, 7, 11, 13, 17, 19};
+        bitset<21> isPrime=0;
+        for(int p: prime) isPrime[p]=1;
+        int sum=0;
+        for(unsigned i=left; i<=right; i++){
+            int b=popcount(i);
+            if (isPrime[b]) sum++;
         }
-
-        return cnt;
+        return sum;
     }
 };
