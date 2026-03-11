58c2fb33ccc06b5b159fafa69b569c718b729638
Hariharan R I <hariharan17kpm@gmail.com>
2026-03-11 21:26:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (90.31%) - LeetHub

--- 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        if(n == 0) return 1 ; 
-        int mask = (1 << (32 - __builtin_clz(n))) - 1; 
-        return n ^ mask ;
+        if (n == 0)
+            return 1;
+        int mask = (1 << (32 - __builtin_clz(n))) - 1;
+        return n ^ mask;
     }
 };
