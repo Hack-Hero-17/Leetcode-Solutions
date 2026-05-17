1021f300bd96eb6c63cb977b62ba310af7a6e214
Hariharan R I <hariharan17kpm@gmail.com>
2026-05-17 19:42:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+
+        vector<bool> visited(n, false);
+        queue<int> q;
+
+        q.push(start);
+
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop();
+
+            if (i < 0 || i >= n || visited[i])
+                continue;
+
+            if (arr[i] == 0)
+                return true;
+
+            visited[i] = true;
+
+            q.push(i + arr[i]);
+            q.push(i - arr[i]);
+        }
+
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
