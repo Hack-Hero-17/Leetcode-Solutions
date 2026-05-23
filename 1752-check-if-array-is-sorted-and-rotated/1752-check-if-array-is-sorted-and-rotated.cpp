90ec41a891f5d8b1b161e1961545fa34bed6d8f0
Hariharan R I <hariharan17kpm@gmail.com>
2026-05-23 21:09:04 +0530

    Time: 0 ms (100%), Space: 11.2 MB (53.59%) - LeetHub - Time: O(?), Space: O(?)

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-         int count = 0, n = nums.size();
-        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % n]) 
-                count++;
-            if (count > 1)
-                return false;
+        const int n=nums.size();
+        int cntD=0;
+        for(int i=1; i<n && cntD<=1; i++){
+            if (nums[i]<nums[i-1]) cntD++;
         }
-        return true;
+        return (nums[0]>=nums.back() && cntD==1) || cntD==0;
+        
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
