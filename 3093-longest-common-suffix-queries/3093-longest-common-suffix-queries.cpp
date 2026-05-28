b867aa94831f068664d26297ee2d5ba3145a7d15
Hariharan R I <hariharan17kpm@gmail.com>
2026-05-28 22:34:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
