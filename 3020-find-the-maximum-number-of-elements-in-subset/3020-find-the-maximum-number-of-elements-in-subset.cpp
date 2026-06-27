3e9f76f8ef45da89829a5e07f3b4512257523cfd
Hariharan R I <hariharan17kpm@gmail.com>
2026-06-27 20:45:38 +0530

    Time: 51 ms (88.57%), Space: 118.7 MB (80.57%) - LeetHub - Time: O(n + m * log(m)), Space: O(n)

---
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (auto& n : nums)
+            freq[n]++;
+
+        int res = (freq[1] - 1) | 1;
+        freq.erase(1);
+
+        for (auto& f : freq) {
+            int n = 0, x = f.first;
+
+            int sq = sqrt(x);
+            if (sq * sq == x && freq.count(sq) && freq[sq] > 1)
+                continue;
+
+            while (x < 31623 && freq.count(x) && freq[x] > 1) {
+                n += 2;
+                x *= x;
+            }
+
+            res = fmax(res, n + ((freq.count(x) << 1) - 1));
+        }
+
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
