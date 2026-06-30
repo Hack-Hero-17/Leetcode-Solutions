9d6fa89a0bac0c06632c42a6004fd68005a017ff
Hariharan R I <hariharan17kpm@gmail.com>
2026-06-30 22:03:04 +0530

    Time: 12 ms (37.16%), Space: 11 MB (57.43%) - LeetHub - Time: O(?), Space: O(?)

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-       vector<int> count(3, 0);  // Count array for 'a', 'b', 'c'
-    int left = 0, right = 0, n = s.size(), result = 0;
+        int res = 0;
+        int p[3] = {-1, -1, -1};
 
-    for (right = 0; right < n; right++) {
-        count[s[right] - 'a']++;  // Increase frequency of current character
-
-        // Shrink the window when all three characters are present
-        while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-            result += (n - right); // Every substring starting from 'left' to 'n' is valid
-            count[s[left] - 'a']--; // Reduce the count of leftmost character
-            left++;  // Move left pointer
+        for (int i = 0; i < s.length(); i++) {
+            p[(s[i] & 31) - 1] = i;
+            res += min({p[0], p[1], p[2]}) + 1;
         }
-    }
 
-    return result; 
+        return res;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
