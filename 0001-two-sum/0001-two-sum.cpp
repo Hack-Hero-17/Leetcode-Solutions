28ef8bd9d8af4577634bda9bcfc2e15f33d850d1
Hariharan R I <hariharan17kpm@gmail.com>
2026-07-25 12:22:52 +0530

    Time: 0 ms (100%), Space: 15 MB (21%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;
-        for( int i=0 ; i< nums.size() ; i++ ){
+        for( int i=0; i<nums.size(); i++){
             int num = nums[i];
             int more = target - num;
-            if( mp.find(more) != mp.end() ){
+            if( mp.find(more) != mp.end() )
                 return { mp[more] , i };
-            }
             mp[num] = i;
         }
         return {};
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
