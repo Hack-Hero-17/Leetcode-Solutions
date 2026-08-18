87c9104033b485bd3088a044c8cd749ff3449b24
Hariharan R I <hariharan17kpm@gmail.com>
2026-08-18 23:39:42 +0530

    Time: 11 ms (74.29%), Space: 21 MB (36.63%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,37 +2,32 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> res;
-
-        for (int n : nums) {
-            if (res.empty() || res.back() < n) {
-                res.push_back(n);
-            } else {
-                int idx = binarySearch(res, n);
-                res[idx] = n;
+        for( int num : nums ){
+            if( res.empty() || res.back() < num ){
+                res.push_back(num);
+            }
+            else{
+                int idx = binary_search(num,res);
+                res[idx] = num;
             }
         }
-
-        return res.size();        
+        return res.size();
     }
-
 private:
-    int binarySearch(const vector<int>& arr, int target) {
-        int left = 0;
-        int right = arr.size() - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
+    int binary_search( int n, vector<int> arr ){
+        int low = 0;
+        int high = arr.size()-1;
+        while( low <= high ){
+            int mid = low + (high-low)/2;
+            if( arr[mid] == n)
                 return mid;
-            } else if (arr[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+            else if( arr[mid] > n )
+                high = mid-1;
+            else
+                low = mid+1;
         }
-
-        return left;
-    }    
+        return low;
+    }
 };
 
 // Synced seamlessly with LeetHub Pro
