4e4d0198ca3f65a904f13c82ad97733c6f61ec69
Hariharan R I <hariharan17kpm@gmail.com>
2026-08-29 21:22:46 +0530

    Time: 241 ms (56.34%), Space: 202.6 MB (44.51%) - LeetHub - Time: O(N log N), Space: O(N)

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,66 +1,33 @@
 class Solution {
 public:
-    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        int n = nums.size();
-        vector<int> parent(n), rank(n);
+    vector<int> lexicographicallySmallestArray(vector<int>& A, int limit) {
+        vector<int> sorted = A;
+        ranges::sort(sorted);
+        vector<vector<int>> grps;
+        unordered_map<int, int> map;
+        int id = -1;
 
-        // Union-Find initialization
-        iota(parent.begin(), parent.end(), 0);
-        fill(rank.begin(), rank.end(), 0);
-
-        // Find function with path compression
-        function<int(int)> find = [&](int x) {
-            if (x != parent[x]) {
-                parent[x] = find(parent[x]);
+        for (int i = 0; i < sorted.size(); i++) {
+            if (i == 0 || sorted[i] - sorted[i - 1] > limit) {
+                grps.push_back({});
+                id++;
             }
-            return parent[x];
-        };
-
-        // Union function with union by rank
-        auto unionSet = [&](int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX != rootY) {
-                if (rank[rootX] < rank[rootY]) {
-                    parent[rootX] = rootY;
-                } else if (rank[rootX] > rank[rootY]) {
-                    parent[rootY] = rootX;
-                } else {
-                    parent[rootY] = rootX;
-                    rank[rootX]++;
-                }
-            }
-        };
-
-        // Build groups of indices that can swap
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (abs(nums[i] - nums[j]) <= limit) {
-                    unionSet(i, j);
-                }
-            }
+            grps[id].push_back(sorted[i]);
+            map[sorted[i]] = id;
         }
 
-        // Group indices based on their roots
-        unordered_map<int, vector<int>> groups;
-        for (int i = 0; i < n; ++i) {
-            groups[find(i)].push_back(i);
+        vector<int> idx(grps.size(), 0);
+
+        for (int i = 0; i < A.size(); i++) {
+            int cur = map[A[i]];
+            A[i] = grps[cur][idx[cur]];
+            idx[cur]++;
         }
 
-        // Sort values within each group
-        vector<int> result = nums;
-        for (auto& [root, indices] : groups) {
-            vector<int> values;
-            for (int idx : indices) {
-                values.push_back(nums[idx]);
-            }
-            sort(values.begin(), values.end());
-            sort(indices.begin(), indices.end());
-            for (int k = 0; k < indices.size(); ++k) {
-                result[indices[k]] = values[k];
-            }
-        }
-
-        return result;
+        return A;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
