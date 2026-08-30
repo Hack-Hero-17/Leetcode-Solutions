da92379189e5e92584e25833dcd696ae3bc13e15
Hariharan R I <hariharan17kpm@gmail.com>
2026-08-30 21:05:59 +0530

    Time: 3 ms (36.81%), Space: 92.1 MB (58.82%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[left])
+                left = i;
+                
+            if (nums[i] > nums[right])
+                right = i;
+        }
+        
+        if (left < right)
+            swap(left, right);
+            
+        int ans = n;
+        
+        for (int i = 0; i <= n; i++) {
+            int extra = 0;
+            
+            if (right >= i)
+                extra = n - right;
+            else if (left >= i)
+                extra = n - left;
+                
+            ans = min(ans, i + extra);
+        }
+        
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
