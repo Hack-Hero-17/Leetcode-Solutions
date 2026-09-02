bf2bb11f5b3900c08b1daac0517bf8f8a56a1a36
Hariharan R I <hariharan17kpm@gmail.com>
2026-09-02 21:43:52 +0530

    Time: 0 ms (100%), Space: 30.2 MB (94.95%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool uniformArray(auto& A) { return 1; }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
