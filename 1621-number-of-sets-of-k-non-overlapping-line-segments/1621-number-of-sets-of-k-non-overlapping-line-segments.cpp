18cf5c5bc63d2004849c3a14fa27a72dbb7602eb
Hariharan R I <hariharan17kpm@gmail.com>
2026-09-16 22:45:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (94.86%) - LeetHub - Time: O(k), Space: O(1)

---
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    static const long long MOD = 1000000007LL;
+
+    long long modPow(long long base, long long exp) {
+        long long result = 1;
+
+        while (exp > 0) {
+            if (exp & 1LL) {
+                result = result * base % MOD;
+            }
+
+            base = base * base % MOD;
+
+            exp >>= 1LL;
+        }
+
+        return result;
+    }
+
+    int numberOfSets(int n, int k) {
+        long long N = n + k - 1;
+        long long R = 2LL * k;
+
+        R = min(R, N - R);
+
+        long long numerator = 1;
+        long long denominator = 1;
+
+        for (long long i = 1; i <= R; ++i) {
+            numerator = numerator * (N - R + i) % MOD;
+
+            denominator = denominator * i % MOD;
+        }
+
+        long long inverseDenominator = modPow(denominator, MOD - 2);
+
+        return static_cast<int>(numerator * inverseDenominator % MOD);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
