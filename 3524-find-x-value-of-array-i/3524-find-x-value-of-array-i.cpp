8f4c6cfdcdf96bfaaee8368b2a9490a8673dcf9d
Hariharan R I <hariharan17kpm@gmail.com>
2026-09-21 23:43:57 +0530

    Time: 7 ms (99.11%), Space: 106.2 MB (94.64%) - LeetHub - Time: O(n * k), Space: O(k)

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& A, int k) {
+        vector<long long> res(k);
+        int freq[5] = {0};
+
+        for (auto& n : A) {
+            n %= k;
+            int cur[5] = {0}; //current freq.
+
+            cur[n] = 1;
+
+            for (int x = 0; x < k; x++)
+                cur[x * n % k] += freq[x];
+
+            for (int x = 0; x < k; x++) {
+                freq[x] = cur[x];
+                res[x] += freq[x];
+            }
+        }
+
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
